73a5c2dabf6c14003adb3c9f1edbb4ca32e5fb3d
Mikhail <mike25122003@gmail.com>
2025-01-09 00:28:57 +0300

    новый путь для запросов + логика добавления продавца в сессию

--- app/blueprints/bp_auth/route.py
+++ app/blueprints/bp_auth/route.py
@@ -1,5 +1,3 @@
-from multiprocessing.forkserver import read_signed
-
 from flask import Blueprint, render_template, request, current_app, session, flash, redirect, url_for
 from werkzeug.security import check_password_hash
 from database import execute_and_fetch, SqlProvider
@@ -7,7 +5,7 @@ from access import already_authenticated
 
 
 bp_auth = Blueprint('bp_auth', __name__, template_folder='templates', static_folder='static')
-provider = SqlProvider('./sql')
+provider = SqlProvider('./blueprints/bp_auth/sql')
 
 
 @bp_auth.route('', methods=['GET'])
@@ -31,7 +29,6 @@ def process_user_auth():
     session['user_group'], session['login'] = result[0]['user_group'], result[0]['login']
     session['user_id'], session['email'] = result[0]['user_id'], result[0]['email']
     flash(f"Вы авторизовались как {session['login']}", 'success')
-    print(session)
     return redirect(url_for('bp_user_menu.user_menu'))
 
 
@@ -48,7 +45,6 @@ def process_staff_auth():
     login = request.form.get('login')
     password = request.form.get('password')
     sql = provider.get_sql('internal_auth.sql', login=login)
-    print(sql)
     result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
     if not result:
         flash('Пользователь не найден', 'error')
@@ -56,15 +52,20 @@ def process_staff_auth():
     if result[0]['password'] != password:
         flash('Неверный пароль', 'error')
         return render_template('auth.html', login_type="staff")
-    session['user_group'], session['login'] = result[0]['user_group'], result[0]['login']
-    session['user_id'] = result[0]['user_id']
+    session['user_id'], session['login'], session['user_group'] = result[0]['user_id'], result[0]['login'], result[0]['user_group']
+
+    #Добавление id продавца в сессию
+    if session['user_group'] == 'cashier':
+        user_id = session['user_id']
+        sql = provider.get_sql('get_cashier_id.sql', user_id=user_id)
+        result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
+        session['cashier_id'] = result[0]['cashier_id']
+
     flash(f"Вы авторизовались как {session['login']}", 'success')
     return redirect(url_for('bp_user_menu.user_menu'))
 
 @bp_auth.route('/logout', methods=['GET'])
 def process_logout():
     session.clear()
-    print(1)
-    print(session)
     flash('Вы вышли из аккаунта', 'success')
     return redirect(url_for('welcome_page'))
