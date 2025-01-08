ac7ebe040119dbde7bea23fbb1560997d272bffb
Mikhail <mike25122003@gmail.com>
2025-01-09 00:30:57 +0300

    изменение структуры запроса

--- app/blueprints/bp_user_orders/sql/view_orders.sql
+++ app/blueprints/bp_user_orders/sql/view_orders.sql
@@ -1,8 +1,11 @@
 SELECT
         o.id AS order_id,
+        o.email,
         o.booking_date,
-        t.ticket_id,
-        t.schedule_id,
+        f.flight_number,
+        s.schedule_date,
+        f.departure_time,
+        f.arrival_time,
         t.passport,
         t.first_name,
         t.last_name,
@@ -12,5 +15,7 @@ SELECT
         t.status
     FROM orders o
     LEFT JOIN tickets t ON o.id = t.order_id
-    WHERE o.user_id = '$user_id'
+    LEFT JOIN schedules s ON t.schedule_id = s.schedule_id
+    LEFT JOIN flights f ON f.flight_id = s.flight_id
+    WHERE o.email = '$email'
     ORDER BY o.booking_date DESC, t.ticket_id;
