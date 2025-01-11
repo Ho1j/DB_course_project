594a84de0843c748d5c0834fb6db2a212da034b4
Mikhail <mike25122003@gmail.com>
2025-01-12 01:49:55 +0300

    завершил работу над маршрутом обновления билетов для кассира

--- app/blueprints/bp_cashier_orders/route.py
+++ app/blueprints/bp_cashier_orders/route.py
@@ -12,6 +12,11 @@ provider = SqlProvider('./blueprints/bp_cashier_orders/sql')
 def pending_orders():
     sql = provider.get_sql("get_booked_orders.sql")
     result = execute_and_fetch(current_app.config["DB_CONFIG"], sql)
+
+    if not result:
+        flash('Заказы не найдены', 'error')
+        return redirect(url_for('bp_user_menu.user_menu'))
+
     orders = {}
     for order in result:
         order_id = order['order_id']
@@ -22,6 +27,7 @@ def pending_orders():
                 'tickets': []
             }
         orders[order_id]['tickets'].append({
+            'ticket_id': order['ticket_id'],
             'flight_number': order['flight_number'],
             'schedule_date': order['schedule_date'],
             'departure_time': order['departure_time'],
@@ -62,6 +68,51 @@ def change_order(order_id):
         execute_and_fetch(current_app.config["DB_CONFIG"], sql)
         return redirect(url_for('bp_user_menu.user_menu'))
 
-    #ТУТ ДОДЕЛАТЬ, ЗАГЛУШКИ
-    #if action == 'change':
-        #sql = provider.get_sql("change.sql", order_id=order_id)
+    if action == 'change':
+        try:
+            ticket_count = int(request.form.get('ticket_count', 0))
+            tickets = []
+
+            for index in range(1, ticket_count + 1):
+                ticket_data = {
+                    'ticket_id': request.form.get(f'tickets[{index}][ticket_id]'),
+                    'first_name': request.form.get(f'tickets[{index}][first_name]'),
+                    'last_name': request.form.get(f'tickets[{index}][last_name]'),
+                    'passport': request.form.get(f'tickets[{index}][passport]'),
+                    'birth_date': request.form.get(f'tickets[{index}][birth_date]'),
+                    'seat_number': request.form.get(f'tickets[{index}][seat_number]'),
+                    'price': request.form.get(f'tickets[{index}][price]'),
+                    'status': request.form.get(f'tickets[{index}][status]'),
+                }
+                tickets.append(ticket_data)
+
+
+            with DBContextManager(current_app.config['DB_CONFIG']) as cursor:
+                for ticket in tickets:
+                    if ticket['status'] == "canceled":
+                        sql = provider.get_sql("transfer_ticket_to_cancelled.sql", ticket_id=ticket['ticket_id'])
+                        print(sql)
+                        cursor.execute(sql)
+                        print(1)
+                        sql = provider.get_sql("delete_ticket.sql", ticket_id=ticket['ticket_id'])
+                        cursor.execute(sql)
+                    else:
+                        sql = provider.get_sql(
+                            "update_ticket.sql",
+                            ticket_id=ticket['ticket_id'],
+                            first_name=ticket['first_name'],
+                            last_name=ticket['last_name'],
+                            passport=ticket['passport'],
+                            birth_date=ticket['birth_date'],
+                            seat_number=ticket['seat_number'],
+                            price=ticket['price'],
+                            status=ticket['status'],
+                        )
+                        cursor.execute(sql)
+
+            flash(f"Заказ №{order_id} успешно обновлен!", "success")
+
+        except Exception as e:
+            flash(f"Ошибка обновления заказа: {str(e)}", "error")
+
+        return redirect(url_for('bp_cashier_orders.pending_orders'))
