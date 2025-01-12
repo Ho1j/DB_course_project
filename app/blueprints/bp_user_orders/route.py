e3868f170eb70e0a34a744b309714524ef65c02c
Mikhail <mike25122003@gmail.com>
2025-01-13 01:37:46 +0300

    перенес реализацию в utils orders

--- app/blueprints/bp_user_orders/route.py
+++ app/blueprints/bp_user_orders/route.py
@@ -1,4 +1,5 @@
 ﻿from flask import Blueprint, session, render_template, current_app
+from utils import create_orders_list
 from database import execute_and_fetch, SqlProvider
 from access import auth_required, group_required
 
@@ -13,28 +14,7 @@ def user_orders():
     email = session.get('email')
     sql = provider.get_sql("view_orders.sql", email=email)
     result = execute_and_fetch(current_app.config["DB_CONFIG"], sql)
-    print(result)
-    orders = {}
-    for order in result:
-        order_id = order['order_id']
-        if order_id not in orders:
-            orders[order_id] = {
-                'booking_date': order['booking_date'],
-                'email': order['email'],
-                'tickets': []
-            }
-        orders[order_id]['tickets'].append({
-            'flight_number': order['flight_number'],
-            'schedule_date': order['schedule_date'],
-            'departure_time': order['departure_time'],
-            'arrival_time': order['arrival_time'],
-            'passport': order['passport'],
-            'first_name': order['first_name'],
-            'last_name': order['last_name'],
-            'birth_date': order['birth_date'],
-            'seat_number': order['seat_number'],
-            'price': order['price'],
-            'status': order['status']
-        })
+
+    orders = create_orders_list(result)
 
     return render_template('user-orders.html', orders=orders)
