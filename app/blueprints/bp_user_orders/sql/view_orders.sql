4fd3489691abf20cc1e3dcb14b4cc7b5bcb262aa
Mikhail <mike25122003@gmail.com>
2025-01-13 01:38:35 +0300

    теперь выводятся отмененные билеты из orders

--- app/blueprints/bp_user_orders/sql/view_orders.sql
+++ app/blueprints/bp_user_orders/sql/view_orders.sql
@@ -1,21 +1,47 @@
 SELECT
-        o.id AS order_id,
-        o.email,
-        o.booking_date,
-        f.flight_number,
-        s.schedule_date,
-        f.departure_time,
-        f.arrival_time,
-        t.passport,
-        t.first_name,
-        t.last_name,
-        t.birth_date,
-        t.seat_number,
-        t.price,
-        t.status
-    FROM orders o
-    LEFT JOIN tickets t ON o.id = t.order_id
-    LEFT JOIN schedules s ON t.schedule_id = s.schedule_id
-    LEFT JOIN flights f ON f.flight_id = s.flight_id
-    WHERE o.email = '$email'
-    ORDER BY o.booking_date DESC, t.ticket_id;
+    o.id AS order_id,
+    o.email,
+    o.booking_date AS booking_date,
+    f.flight_number,
+    s.schedule_date,
+    f.departure_time,
+    f.arrival_time,
+    t.passport,
+    t.first_name,
+    t.last_name,
+    t.birth_date,
+    t.seat_number,
+    t.price,
+    t.status,
+    t.ticket_id AS ticket_id
+FROM orders o
+LEFT JOIN tickets t ON o.id = t.order_id
+LEFT JOIN schedules s ON t.schedule_id = s.schedule_id
+LEFT JOIN flights f ON f.flight_id = s.flight_id
+WHERE o.email = '$email'
+
+UNION ALL
+
+SELECT
+    o.id AS order_id,
+    o.email,
+    o.booking_date AS booking_date,
+    f.flight_number,
+    s.schedule_date,
+    f.departure_time,
+    f.arrival_time,
+    t.passport,
+    t.first_name,
+    t.last_name,
+    t.birth_date,
+    t.seat_number,
+    t.price,
+    t.status,
+    t.ticket_id AS ticket_id
+FROM orders o
+LEFT JOIN canceled_tickets t ON o.id = t.order_id
+LEFT JOIN schedules s ON t.schedule_id = s.schedule_id
+LEFT JOIN flights f ON f.flight_id = s.flight_id
+WHERE o.email = '$email'
+
+ORDER BY booking_date DESC, ticket_id;
