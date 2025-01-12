fd7015c0a52f52d5d3800a2a24c2ebc34756a0de
Mikhail <mike25122003@gmail.com>
2025-01-13 01:37:00 +0300

    запрос теперь вставляет в билет цену из flights

--- app/blueprints/bp_booking/sql/create_ticket.sql
+++ app/blueprints/bp_booking/sql/create_ticket.sql
@@ -5,7 +5,8 @@ INSERT INTO tickets (
     first_name,
     last_name,
     birth_date,
-    seat_number
+    price,
+    status
 )
 VALUES (
     '$order_id',
@@ -14,5 +15,9 @@ VALUES (
     '$first_name',
     '$last_name',
     '$birth_date',
-    '$seat_number'
+    (SELECT f.ticket_price
+     FROM flights f
+     JOIN schedules s ON f.flight_id = s.flight_id
+     WHERE s.schedule_id = '$schedule_id'),
+    '$status'
 );
