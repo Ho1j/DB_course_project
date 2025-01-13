a3d48083f3a2b8c55b3f192464e30e452c673dce
Mikhail <mike25122003@gmail.com>
2025-01-13 17:19:09 +0300

    новые запросы

--- app/blueprints/bp_queries/route.py
+++ app/blueprints/bp_queries/route.py
@@ -1,49 +1,78 @@
-from flask import Blueprint, render_template, request, current_app, session, redirect, url_for
-import json
+from flask import Blueprint, render_template, request, current_app, session, redirect, url_for, flash
+from utils import create_orders_list
 from database import execute_and_fetch, SqlProvider
 from access import auth_required, group_required
 
 bp_queries = Blueprint('bp_queries', __name__, template_folder='templates', static_folder='static')
-provider = SqlProvider('./sql')
-with open('blueprints/bp_queries/static/imgs/products.json') as f:
-    products_imgs = json.load(f)
+provider = SqlProvider('./blueprints/bp_queries/sql')
 
 
-@bp_queries.route('/mytickets')
+@bp_queries.route('/menu')
 @auth_required
 @group_required
-def search_choice():
-    print(session)
-    return render_template('search_choice.html')
+def queries_menu():
+    user_group = session.get('user_group')
+    login = session.get('login')
+    return render_template('queries-menu.html', user_group=user_group, login=login)
 
-@bp_queries.route('/input_price', methods=['GET', 'POST'])
-def input_price():
-    if request.method == 'GET':
-        find_categories = provider.get_sql('categories.sql')
-        categories = execute_and_fetch(current_app.config['DB_CONFIG'], find_categories)
-        return render_template('input_price.html', categories=categories)
-    else:
-        category = request.form.get('category')
-        price = request.form.get('price')
-        sql = provider.get_sql('queries.sql', category=category, price=price)
 
-        products = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
-        return render_template('results.html', prod_title=f"Продукты категории {category} с ценой до {price} руб",
-                               products=products, products_imgs=products_imgs, page='input_price')
+@bp_queries.route('/search-orders-by-mail', methods=['GET'])
+@auth_required
+@group_required
+def search_orders_by_mail():
+    return render_template('search-orders-by-email.html')
 
-@bp_queries.route('/input_market', methods=['GET', 'POST'])
-def input_market():
-    markets = {'на Тверской': 'Tverskaya', 'на Маяковской': 'Mayakovskaya', 'на Курской': 'Kurskaya',
-               'на Рижской': 'Rizhskaya', 'онлайн': 'online'}
-    if request.method == 'GET':
-        find_categories = provider.get_sql('categories.sql')
-        categories = execute_and_fetch(current_app.config['DB_CONFIG'], find_categories)
-        return render_template('input_market.html', markets=markets.keys(), categories=categories)
-    else:
-        market_rus = request.form.get('market')
-        market = markets[market_rus]
-        category = request.form.get('category')
-        sql = provider.get_sql('market.sql', market=market, category=category)
-        products = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
-        return render_template('results.html', prod_title=f"Продукты категории {category} в наличии в магазине {market_rus}",
-                               products=products, products_imgs=products_imgs, page='input_market')
+@bp_queries.route('/search-orders-by-mail/results', methods=['GET'])
+@auth_required
+@group_required
+def show_orders_by_email():
+    email = request.args.get('email')
+    sql = provider.get_sql('get_orders_by_email.sql', email=email)
+    get_orders_by_mail_result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
+    if not get_orders_by_mail_result:
+        flash(f"Заказы с почтой {email} не найдены", "error")
+        return redirect(url_for('bp_queries.queries_menu'))
+    orders = create_orders_list(get_orders_by_mail_result)
+    return render_template('show-orders.html', orders=orders)
+
+
+@bp_queries.route('/search-flights-by-month', methods=['GET'])
+@auth_required
+@group_required
+def search_flights_by_month():
+    return render_template('search-flights-by-month.html')
+
+
+@bp_queries.route('/search-flights-by-month/results', methods=['GET'])
+@auth_required
+@group_required
+def show_flights_by_month():
+    month = request.args.get('month')
+    sql = provider.get_sql('get_flights_by_month.sql', month=month)
+    get_flights_by_month_result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
+    if not get_flights_by_month_result:
+        flash(f"Заказы за {month} не найдены", "error")
+        return redirect(url_for('bp_queries.queries_menu'))
+    return render_template('show-flights.html', result=get_flights_by_month_result)
+
+
+@bp_queries.route('/search-flights-by-countries', methods=['GET'])
+@auth_required
+@group_required
+def search_flights_by_countries():
+    return render_template('search-flights-by-countries.html')
+
+
+@bp_queries.route('/search-flights-by-countries/results', methods=['GET'])
+@auth_required
+@group_required
+def show_flights_by_countries():
+    departure_country = request.args.get('departure-country')
+    arrival_country = request.args.get('arrival-country')
+    sql = provider.get_sql('get_flights_by_countries.sql', departure_country=departure_country, arrival_country=arrival_country)
+    get_flights_by_countries_result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
+    if not get_flights_by_countries_result:
+        flash(f"Рейсы из {departure_country} в {arrival_country} не найдены", "error")
+        return redirect(url_for('bp_queries.queries_menu'))
+    return render_template('show-flights.html', result=get_flights_by_countries_result)
+
---
+++ app/blueprints/bp_queries/sql/get_flights_by_countries.sql
@@ -0,0 +1,25 @@
+SELECT
+    schedule_id,
+    f.flight_number,
+    dep_airport.airport_name AS departure_airport,
+    dep_airport.city AS departure_city,
+    dep_airport.country AS departure_country,
+    s.schedule_date AS departure_date,
+    f.departure_time AS departure_time,
+    arr_airport.airport_name AS arrival_airport,
+    arr_airport.city AS arrival_city,
+    arr_airport.country AS arrival_country,
+    f.ticket_price,
+    f.arrival_time AS arrival_time
+FROM
+    flights f
+JOIN
+    schedules s ON f.flight_id = s.flight_id
+JOIN
+    airports dep_airport ON f.departure_airport_id = dep_airport.airport_id
+JOIN
+    airports arr_airport ON f.arrival_airport_id = arr_airport.airport_id
+WHERE
+    dep_airport.country = '$departure_country' AND arr_airport.country = '$arrival_country'
+ORDER BY
+    s.schedule_date
